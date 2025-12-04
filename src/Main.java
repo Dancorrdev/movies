1a018be7e44186d49ac4cc78489a9c9afc2e2539
Danilo Andres Cortes Lopez <da.cortesl97@gmail.com>
2025-12-04 02:49:45 -0500

    chore: add menu to navigate through platform

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,67 @@ import util.ScannerUtils;
 public class Main {
     public static final String NOMBRE_PLATAFORMA = "🍿🍿🍿 Stream Dev 🎥🎥🎥";
     public static final String VERSION = "1.0.0";
+    public static final int AGREGAR_CONTENIDO = 1;
+    public static final int MOSTRAR_TODO = 2;
+    public static final int BUSCAR_POR_TITULO = 3;
+    public static final int ELIMINAR_CONTENIDO = 4;
+    public static final int SALIR = 5;
 
     public static void main(String[] args) {
         Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
         System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
-        String title = ScannerUtils.catchText("Nombre del contenido");
-        String genre = ScannerUtils.catchText("Genero del contenido");
-        int length = ScannerUtils.catchNumber("Duracion del contenido");
-        double rating = ScannerUtils.catchDecimal("Calificacion del Contenido");
 
-        Pelicula pelicula = new Pelicula(title, length, genre, rating);
-        Pelicula pelicula1 = new Pelicula("The Matrix", 136, "Ciencia Ficción", 4.7);
-        Pelicula pelicula2 = new Pelicula("Inception", 148, "Acción", 4.8);
+        while (true){
+            int selectedOption = ScannerUtils.catchNumber("""
+                            Seleccione una opción:
+                            1. Agregar contenido
+                            2. Mostrar todo
+                            3. Buscar por título
+                            4. Eliminar contenido
+                            5. Salir"""
+            );
+            switch (selectedOption) {
+                case AGREGAR_CONTENIDO -> {
+                    String title = ScannerUtils.catchText("Nombre del contenido");
+                    String genre = ScannerUtils.catchText("Genero del contenido");
+                    int length = ScannerUtils.catchNumber("Duracion del contenido");
+                    double rating = ScannerUtils.catchDecimal("Calificacion del Contenido");
 
-        plataforma.add(pelicula);
-        plataforma.add(pelicula1);
-        plataforma.add(pelicula2);
+                    plataforma.add(new Pelicula(title, length, genre, rating));
+                    System.out.println("Contenido agregado exitosamente.");
+                }
+                case MOSTRAR_TODO -> plataforma.showMovies();
 
-        System.out.println("Numero de elementos en la plataforma: " + plataforma.getContenido().size());
-        plataforma.remove(pelicula1);
+                case BUSCAR_POR_TITULO -> {
+                    //falta implementar
+                }
+                case ELIMINAR_CONTENIDO ->   plataforma.remove(null); //falta implementar
 
-        plataforma.showMovies();
+                case SALIR -> {
+                    System.out.println("Saliendo de "+ NOMBRE_PLATAFORMA + ". ¡Hasta luego!");
+                    System.exit(0);
+                }
+            }
 
+        }
 
-        Usuario usuario = new Usuario("Jorge Gonzalez", "Jorge@devstream.com");
+//        Pelicula pelicula = new Pelicula(title, length, genre, rating);
+//        Pelicula pelicula1 = new Pelicula("The Matrix", 136, "Ciencia Ficción", 4.7);
+//        Pelicula pelicula2 = new Pelicula("Inception", 148, "Acción", 4.8);
+//
+//        plataforma.add(pelicula);
+//        plataforma.add(pelicula1);
+//        plataforma.add(pelicula2);
 
-
-        usuario.watch(pelicula);
+//        System.out.println("Numero de elementos en la plataforma: " + plataforma.getContenido().size());
+//
+//
+//
+//        Usuario usuario = new Usuario("Jorge Gonzalez", "Jorge@devstream.com");
+//
+//
+//        usuario.watch(pelicula);
 
     }
 }
