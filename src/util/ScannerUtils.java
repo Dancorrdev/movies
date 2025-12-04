9db500fafbd00ede1a4cb5d566f692dd53a952de
Danilo Andres Cortes Lopez <da.cortesl97@gmail.com>
2025-12-04 02:51:33 -0500

    chore: doing ways to catch posibles errors in typos of menu of the platform

--- src/util/ScannerUtils.java
+++ src/util/ScannerUtils.java
@@ -12,6 +12,11 @@ public class ScannerUtils {
 
     public static int catchNumber ( String mensaje){
         System.out.println(mensaje + ": ");
+
+        while (!SCANNER.hasNextInt()) {
+            System.out.println("Por favor ingrese un número válido." + mensaje + ": ");
+            SCANNER.next();
+        }
         int dato = SCANNER.nextInt();
         SCANNER.nextLine();
         return dato;
@@ -19,6 +24,10 @@ public class ScannerUtils {
 
     public static double catchDecimal ( String mensaje){
         System.out.println(mensaje + ": ");
+        while (!SCANNER.hasNextDouble()) {
+            System.out.println("Por favor ingrese un número decimal válido." + mensaje + ": ");
+            SCANNER.next();
+        }
         double dato = SCANNER.nextDouble();
         SCANNER.nextLine();
         return dato;
