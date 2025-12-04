7b0d3d304ced532468f336313788dcd89142f5fa
Danilo Andres Cortes Lopez <da.cortesl97@gmail.com>
2025-12-04 02:04:31 -0500

    chore: add Plataforma class for sort content into it

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import contenido.Pelicula;
+import plataforma.Plataforma;
 import plataforma.Usuario;
 import util.ScannerUtils;
 
@@ -7,18 +8,26 @@ public class Main {
     public static final String VERSION = "1.0.0";
 
     public static void main(String[] args) {
+        Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
         System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
-        String title = ScannerUtils.catchText("nombre del contenido");
+        String title = ScannerUtils.catchText("Nombre del contenido");
         String genre = ScannerUtils.catchText("Genero del contenido");
         int length = ScannerUtils.catchNumber("Duracion del contenido");
         double rating = ScannerUtils.catchDecimal("Calificacion del Contenido");
 
+        Pelicula pelicula = new Pelicula(title, length, genre, rating);
+        Pelicula pelicula1 = new Pelicula("The Matrix", 136, "Ciencia Ficción", 4.7);
+        Pelicula pelicula2 = new Pelicula("Inception", 148, "Acción", 4.8);
 
-        Pelicula pelicula = new Pelicula(title, length, genre);
-        pelicula.rate(rating);
+        plataforma.add(pelicula);
+        plataforma.add(pelicula1);
+        plataforma.add(pelicula2);
 
-        System.out.println(pelicula.getFactSheet());
+        System.out.println("Numero de elementos en la plataforma: " + plataforma.getContenido().size());
+        plataforma.remove(pelicula1);
+
+        plataforma.showMovies();
 
 
         Usuario usuario = new Usuario("Jorge Gonzalez", "Jorge@devstream.com");
---
+++ src/plataforma/Plataforma.java
@@ -0,0 +1,38 @@
+package plataforma;
+
+import contenido.Pelicula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plataforma {
+    private String name;
+    private List<Pelicula> contenido;
+
+    public Plataforma(String name){
+        this.name = name;
+        this.contenido = new ArrayList<>();
+    }
+
+    public void add(Pelicula pelicula){
+        contenido.add(pelicula);
+    }
+
+    public void showMovies(){
+        for (Pelicula pelicula : contenido) {
+            System.out.println(pelicula.getTitle());
+        }
+    }
+
+    public void remove(Pelicula pelicula){
+        contenido.remove(pelicula);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Pelicula> getContenido() {
+        return contenido;
+    }
+}
