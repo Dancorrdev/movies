61f1bb0097054fd5caddedbfba9b4457fa1a4336
Danilo Andres Cortes Lopez <da.cortesl97@gmail.com>
2025-12-17 13:21:43 -0500

    chore: add use cases for find by title and genre of movies

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,19 @@ import plataforma.Plataforma;
 import plataforma.Usuario;
 import util.ScannerUtils;
 
+import java.util.List;
+
 public class Main {
     public static final String NOMBRE_PLATAFORMA = "🍿🍿🍿 Stream Dev 🎥🎥🎥";
     public static final String VERSION = "1.0.0";
     public static final int AGREGAR_CONTENIDO = 1;
     public static final int MOSTRAR_TODO = 2;
     public static final int BUSCAR_POR_TITULO = 3;
-    public static final int ELIMINAR_CONTENIDO = 4;
-    public static final int SALIR = 5;
+    public static final int BUSCAR_POR_GENERO = 4;
+    public static final int VER_POPULARES = 5;
+    public static final int VER_MAS_LARGO = 6;
+    public static final int ELIMINAR_CONTENIDO = 8;
+    public static final int SALIR = 9;
 
     public static void main(String[] args) {
         Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
@@ -18,14 +23,19 @@ public class Main {
 
         loadMovies(plataforma);
 
+        System.out.println("Mas de " + plataforma.getDuracionTotal() + " minutos de contenido disponible.");
+
         while (true) {
             int selectedOption = ScannerUtils.catchNumber("""
                     Seleccione una opción:
                     1. Agregar contenido
                     2. Mostrar todo
                     3. Buscar por título
-                    4. Eliminar contenido
-                    5. Salir
+                    4. Buscar por género
+                    5. Ver populares
+                    6. Ver contenido más largo
+                    8. Eliminar contenido
+                    9. Salir
                     """
             );
             switch (selectedOption) {
@@ -49,6 +59,28 @@ public class Main {
                         System.out.println(pelicula.getFactSheet());
                 }else {
                         System.out.println("Contenido no encontrado dentro de" + NOMBRE_PLATAFORMA + ".");
+                    }
+                }
+                case BUSCAR_POR_GENERO -> {
+                    String generoDelContenido = ScannerUtils.catchText("Ingrese el género del contenido a buscar");
+                    List<Pelicula> contenidoPorGenero = plataforma.findByGenre(generoDelContenido);
+                    System.out.println(contenidoPorGenero.size() + " Contenidos encontrados en el género " + generoDelContenido);
+                    contenidoPorGenero.forEach(contenido -> System.out.println(contenido.getFactSheet() + "\n"));
+                }
+                case VER_POPULARES -> {
+                    int cantidad = ScannerUtils.catchNumber("¿Cuántos contenidos populares desea ver?");
+
+                    List<Pelicula> contenidosPopulares = plataforma.getPopularContent(cantidad);
+                    System.out.println("Top " + contenidosPopulares.size() + " contenidos populares:");
+                    contenidosPopulares.forEach(contenido -> System.out.println(contenido.getFactSheet() + "\n"));
+                }
+                case VER_MAS_LARGO -> {
+                    Pelicula peliculaMasLarga = plataforma.getLongestMovie();
+                    if (peliculaMasLarga != null) {
+                        System.out.println("El contenido más largo es: +\n");
+                        System.out.println(peliculaMasLarga.getFactSheet() + "\n");
+                    } else {
+                        System.out.println("No hay contenido disponible en la plataforma.");
                     }
                 }
                 case ELIMINAR_CONTENIDO -> {
@@ -81,5 +113,16 @@ public class Main {
         plataforma.add(new Pelicula("The Matrix", 136, "Sci-Fi", 4.7));
         plataforma.add(new Pelicula("Interstellar", 169, "Sci-Fi", 4.5));
         plataforma.add(new Pelicula("Gladiator", 155, "Action", 4.4));
+        plataforma.add(new Pelicula("Star Wars: Episode IV - A New Hope", 121, "Sci-Fi", 4.6));
+        plataforma.add(new Pelicula("Star Wars: Episode III - Revenge of the Sith", 140, "Sci-Fi", 4.2));
+        plataforma.add(new Pelicula("Avengers: Endgame", 181, "Action", 4.7));
+        plataforma.add(new Pelicula("Harry Potter and the Secret of the Chamber", 161, "Fantasy", 4.5));
+        plataforma.add(new Pelicula("Justice League Snyder Cut", 242, "Action", 4.1));
+        plataforma.add(new Pelicula("Spiman : Into the Spider-Verse", 117, "Animada", 4.8));
+        plataforma.add(new Pelicula("Dragon Ball Evolution", 85, "Action", 2.3));
+        plataforma.add(new Pelicula("Catwoman", 104, "Action", 1.9));
+        plataforma.add(new Pelicula("John Carter", 132, "Sci-Fi", 3.1));
+
+
     }
 }
--- src/plataforma/Plataforma.java
+++ src/plataforma/Plataforma.java
@@ -3,6 +3,7 @@ package plataforma;
 import contenido.Pelicula;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Plataforma {
@@ -19,22 +20,42 @@ public class Plataforma {
     }
 
     public void showMovies(){
-        for (Pelicula pelicula : contenido) {
-            System.out.println(pelicula.getTitle());
-        }
+        contenido.forEach(pelicula -> System.out.println(pelicula.getTitle()));
     }
 
     public void remove(Pelicula pelicula){
         contenido.remove(pelicula);
     }
 
-    public Pelicula findByTitle(String title){
-        for (Pelicula pelicula : contenido) {
-            if (pelicula.getTitle().equalsIgnoreCase(title)) {
-                return pelicula;
-            }
-        }
-        return null;
+    public Pelicula findByTitle(String title) {
+       return contenido.stream()
+                .filter(contenido -> contenido.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Pelicula> findByGenre(String genre){
+        return contenido.stream()
+                .filter(contenido-> contenido.getGenre().equalsIgnoreCase(genre))
+                .toList();
+    }
+
+    public int getDuracionTotal(){
+        return contenido.stream()
+                .mapToInt(Pelicula::getLength)
+                .sum();
+    }
+
+    public List<Pelicula> getPopularContent(int cantidad){
+        return contenido.stream().sorted(Comparator.comparingDouble(Pelicula::getRating).reversed())
+                .limit(cantidad)
+                .toList();
+    }
+
+    public Pelicula getLongestMovie(){
+        return contenido.stream()
+                .max(Comparator.comparingInt(Pelicula::getLength))
+                .orElse(null);
     }
 
     public String getName() {
